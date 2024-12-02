f5c04a7b0edbdadeacda25a6a39ba3f1c9943104
Vishal M Yadav <vishal.yadav@embibe.com>
2024-12-02 08:58:41 +0530

    added to do problems for more practice

---
+++ src/main/java/com/gatomalvado/todo/executorservice/Main.java
@@ -0,0 +1,9 @@
+package com.gatomalvado.todo.executorservice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Executor service!");
+    }
+
+}
---
+++ src/main/java/com/gatomalvado/todo/foodkart/Main.java
@@ -0,0 +1,9 @@
+package com.gatomalvado.todo.foodkart;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Food Kart!");
+    }
+
+}
